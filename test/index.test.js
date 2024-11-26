cb3220a8da37d808ba58403e2e8bc74d40242648
diegodonascimentoconfessor <153656081+diegodonascimentoconfessor@users.noreply.github.com>
2024-11-26 15:04:05 -0300

    att

--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,14 @@
 import { salariodesconto, salariototal } from "..";
 
 describe('Cálculo Geral do Salário', () => {
-    test('deve retornar o salário total', () => {
+    it('deve retornar o salário total', () => {
         const esperado = 1892;
         const retornado = salariototal(1412, 300, 180);
 
         expect(retornado).toBe(esperado);
     });
 
-    test('deve descontar o valor do salário', () => {
+    it('deve descontar o valor do salário', () => {
         const esperado = 2600;
         const retornado = salariodesconto(3000, 400);
 
