05aef1130ea5a989f0220dcd5010a71642665d3c
diegodonascimentoconfessor <153656081+diegodonascimentoconfessor@users.noreply.github.com>
2024-11-28 13:48:12 -0300

    att

--- verificarpalindromo.js
+++ verificarpalindromo.js
@@ -1,6 +1,4 @@
-function verificarPalindromo(texto) {
-    const textoLimpo = texto.toLowerCase().replace(/[^a-z0-9]/g, ''); // Remove caracteres especiais e espaços
-    return textoLimpo === textoLimpo.split('').reverse().join(''); // Verifica se é igual ao reverso
+export function verificarPalindromo(texto) {
+    const textoNormalizado = texto.toLowerCase().replace(/[^a-z0-9]/g, '');
+    return textoNormalizado === textoNormalizado.split('').reverse().join('');
 }
-
-export { verificarPalindromo };
